55fe876ab81d73fc2cfae04206d9e4a5820150ce
Ludivine NOVILLO <lunovill@e1r12p21.clusters.42paris.fr>
2022-10-14 21:31:12 +0200

    lunovill

--- objets/lst_add.d
+++
@@ -1,8 +0,0 @@
-objets/lst_add.o: sources/lst_add.c includes/list.h \
-  libft/includes/libft.h libft/includes/ft_printf.h
-
-includes/list.h:
-
-libft/includes/libft.h:
-
-libft/includes/ft_printf.h:
