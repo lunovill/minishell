fc51e3a5d7212c44562907aaee84ae5c68248f1a
Ludivine NOVILLO <lunovill@e3r10p3.clusters.42paris.fr>
2022-10-24 21:06:27 +0200

    parsing

--- objets/lst_new.d
+++
@@ -1,8 +0,0 @@
-objets/lst_new.o: sources/lst_new.c includes/list.h \
-  libft/includes/libft.h libft/includes/ft_printf.h
-
-includes/list.h:
-
-libft/includes/libft.h:
-
-libft/includes/ft_printf.h:
