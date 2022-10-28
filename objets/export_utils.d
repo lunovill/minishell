ef28ab32465e16e364d3ed8463d1ac760970f371
sofia ! <sev.jocks@yahoo.fr>
2022-10-29 01:30:46 +0200

    fixed : list -> char *

--- objets/export_utils.d
+++
@@ -1,8 +0,0 @@
-objets/export_utils.o: sources/export_utils.c includes/list.h \
-  libft/includes/libft.h libft/includes/ft_printf.h
-
-includes/list.h:
-
-libft/includes/libft.h:
-
-libft/includes/ft_printf.h:
--- objets/signals_utils.d
+++
@@ -1,8 +0,0 @@
-objets/signals_utils.o: sources/signals_utils.c includes/list.h \
-  libft/includes/libft.h libft/includes/ft_printf.h
-
-includes/list.h:
-
-libft/includes/libft.h:
-
-libft/includes/ft_printf.h:
